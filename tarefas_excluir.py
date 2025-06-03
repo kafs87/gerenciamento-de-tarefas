855b0497da2aa0136958afbb42e8be5ab4560b19
kafs87 <kauanferreirakk@gmail.com>
2025-06-02 21:22:36 -0300

    Exclusão de tarefas criado

--- tarefas_excluir.py
+++ tarefas_excluir.py
@@ -5,7 +5,27 @@
 
 # Bibliotecas
 from dicionario import tarefas, proximo_id # Importa tarefas do dicionario global
+import tarefas_visualizar
 
 def excluir():
     print('\n--- Exclusão de Tarefas ---\n')
-    
+    tarefas_visualizar.visualizacao_simples()
+    id = int(input('Identificador da Tarefa: '))
+
+    # Consultar para excluir
+    while True:
+        if id in tarefas:
+            resposta = input(f'Você realmente deseja excluir a tarefa {id}? (s/n)').strip().lower()
+
+            while resposta not in ['n', 'nao', 'não', 's', 'sim']:
+                resposta = input(f'Resposta inválida. Você realmente deseja excluir o paciente {id}? (s/n)')
+
+            if resposta in ['s', 'sim']:
+                del tarefas[id]
+                print(f'Tarefa {id} excluído com sucesso.')
+                break
+            else:
+                print('Tarefa não excluída.')
+                break
+        else: 
+            print(f'Tarefa {id} não encontrada.')
